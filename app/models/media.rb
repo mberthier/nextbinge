9dc44f67631cc25bf8c2aff81028abceedfbb850
Maxime Berthier <41864957+mberthier@users.noreply.github.com>
2020-08-20 22:42:04 +0200

    Update media.rb

--- app/models/media.rb
+++ app/models/media.rb
@@ -2,4 +2,10 @@ class Media < ApplicationRecord
   belongs_to :media_user
   validates :title, :plot, :release_year, :ratings, :genre, :poster, :media_type, :streaming_service, presence: true
   validates :title, uniqueness: true
+  validates :media_type, inclusion: { in: ["Movies", "TV"] }
+  validates :genre, inclusion: { in: [
+    "Action & Adventure", "Animation", "Anime", "Biography", "Children", "Comedy",
+    "Crime", "Cult", "Documentary", "Drama", "Family", "Fantasy", "History", "Horror", "Independant", "Musical",
+    "Mystery", "Romance", "Sci-Fi", "Sport", "Standup & Talk", "Thriller"
+  ] }
 end
