4f3f950a6ebb5b7d56fd363c0c41a6658a3e3054
Michael Knott <knottweb@gmail.com>
2020-08-22 12:28:50 +0100

    Routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :user, only: :show do
+    resources :survey, only: [:show, :index]
+    resources :media_user, only: [:index, :create, :update, :destroy] do
+      collection do
+        get :bookmarked_index
+        get :watched_index
+        get :excluded_index
+      end
+    end
+  end
+  resources :media, only: :create
 end
