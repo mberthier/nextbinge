cda7fecdbff0d87ae71489b556056ecd72fbc390
Maxime Berthier <berthiermaxime@orange.fr>
2020-08-22 18:50:44 +0200

    created user controller + view | added custom route and action from the pages controller"

---
+++ app/controllers/medias_controller.rb
@@ -0,0 +1,10 @@
+class MediasController < ApplicationController
+
+  def create
+    # :title, :plot, :release_year, :ratings, :genre, :poster, :media_type, :streaming_service
+  end
+
+  def show
+    @media = Media.find(params[:id])
+  end
+end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,75 @@
+require 'open-uri'
+require 'nokogiri'
+require 'net/http'
+require 'openssl'
+require 'json'
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+  end
+
+  def result
+    @survey = current_user.surveys.last
+    @media = @survey.media_type
+    services = ["netflix", "amazon", "disney"]
+    reco_movies = services.map do |service|
+      scrape_by_service(service)
+    end
+    @reco_movies = reco_movies.flatten
+
+    # iterate other the @reco_movies  and run the IMDB API
+  end
+
+  private
+
+  def scrape(streaming_service)
+    @platform = streaming_service
+    genre = {
+      "Action & Adventure": 5,
+      "Animatio": 6,
+      "Anim": 39,
+      "Biograph": 7,
+      "Children": 8,
+      "Comedy": 9,
+      "Crime": 10,
+      "Cult": 41,
+      "Documentary": 11,
+      "Drama": 3,
+      "Family": 12,
+      "Fantasy": 13,
+      "History": 17,
+      "Horor": 19,
+      "Independent": 43,
+      "Musical": 22,
+      "Mystery": 23,
+      "Romance": 4,
+      "Sci-Fi": 26,
+      "Sport": 29,
+      "Standup & Talk": 45,
+      "Thriller": 32
+    }
+    @genre = genre[@survey.genre]
+    @rating = @survey.ratings
+    @year = @survey.release_year
+    url = "https://reelgood.com/uk/#{@media}/source/#{@platform}?filter-genre=#{@genre}&filter-imdb_start=#{@rating}&filter-year_start=#{@year}"
+
+    movies = []
+
+    html_file = open(url).read
+    html_doc = Nokogiri::HTML(html_file)
+    html_doc.search('tr a').each do |element|
+      movies << Media.new( title: element.inner_text, streaming_service: streaming_service)
+    end
+
+    movies = movies.drop(1).reject(&:empty?)
+    movies = movies.sample(5)
+  end
+
+  def scrape_by_service(service)
+    if @survey[service]
+      scrape(service)
+    end
   end
 end
