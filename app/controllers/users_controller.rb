5ef80d239dd0668167b3b6a3b3af478d5cfdf7e3
Rosemary Akl <aklrosemary1@gmail.com>
2020-08-27 20:17:58 +0100

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,48 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+    # before_action :correct_user?, only: [:edit, :update, :destroy]
+
+
+
+  def index
+      @users = User.all
+    end
+
   def show
     @user = User.find(params[:id])
   end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+
+
+
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to users_path
+      else
+        render action: :edit
+      end
+    end
+
+
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      sign_out
+      redirect_to root_path
+    end
+
+  end
+
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+
 end
