36fd9b21236320ed575d912b121f1a47f3c260af
Michael Knott <knottweb@gmail.com>
2020-09-04 00:45:56 +0100

    Added tabs to user show page

--- db/migrate/20200822101009_create_media_users.rb
+++ db/migrate/20200822101009_create_media_users.rb
@@ -1,7 +1,7 @@
 class CreateMediaUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :media_users do |t|
-      t.references :media, null: false, foreign_key: true
+      t.references :media, null: false, foreign_key: true, 
       t.references :user, null: false, foreign_key: true
       t.boolean :bookmarked, default: false
       t.boolean :excluded, default: false
