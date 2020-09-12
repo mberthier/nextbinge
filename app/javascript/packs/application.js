5a163496b6e8441d7c13597bd46e9d1315c926e5
Maxime Berthier <berthiermaxime@orange.fr>
2020-09-12 13:26:53 +0200

    testing the modal of the show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,35 +7,35 @@ require("channels")
 import "bootstrap";
 import getImdbPoster from "../../assets/javascript/imdbApiCall";
 import bookmark from "../components/bookmark";
-import { tns } from "../../../node_modules/tiny-slider/src/tiny-slider";
+// import { tns } from "../../../node_modules/tiny-slider/src/tiny-slider";
 
 document.addEventListener('turbolinks:load', () => {
   getImdbPoster();
   bookmark();
 
-  const sessionFIlterButtons = document.querySelector(".align-banner")
-  if(sessionFIlterButtons) {
-    const slider = tns({
-    container: '.my-slider',
-    loop: true,
-    items: 1,
-    slideBy: "page",
-    nav: false,
-    autoplayButtonOutput: false,
-    mouserDrag: true,
-    lazyload: true,
-    controlsContainer: "#customize-controls",
-    responsive: {
-      640: {
-        items: 2
-      },
-      700: {
-        gutter: 30
-      },
-      900: {
-        items: 3
-      }
-    }
-  });
-  }
+  // const sessionFIlterButtons = document.querySelector(".align-banner")
+  // if(sessionFIlterButtons) {
+  //   const slider = tns({
+  //   container: '.my-slider',
+  //   loop: true,
+  //   items: 1,
+  //   slideBy: "page",
+  //   nav: false,
+  //   autoplayButtonOutput: false,
+  //   mouserDrag: true,
+  //   lazyload: true,
+  //   controlsContainer: "#customize-controls",
+  //   responsive: {
+  //     640: {
+  //       items: 2
+  //     },
+  //     700: {
+  //       gutter: 30
+  //     },
+  //     900: {
+  //       items: 3
+  //     }
+  //   }
+  // });
+  // }
 });
