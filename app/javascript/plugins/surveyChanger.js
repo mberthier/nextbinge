8c36e4a9d6907f7bee1ac97584624f561d2784ba
Michael Knott <knottweb@gmail.com>
2020-09-14 13:15:15 +0100

    Refactoed js and css

--- app/javascript/plugins/surveyChanger.js
+++ app/javascript/plugins/surveyChanger.js
@@ -2,12 +2,12 @@
 const formGroupOneAction = () => {
   const formGroupOne = document.querySelector("fieldset.form-group-1");
   const radioButtonOne = formGroupOne.querySelectorAll(".radio__label");
-  const formGroupTwo = document.querySelector("fieldset.form-group-2-hide");
+  const formGroupTwo = document.querySelector("fieldset.form-group-2-hidden");
   radioButtonOne.forEach(element => {
     element.addEventListener('click', function() {
       formGroupOne.classList.remove("form-group-1");
-      formGroupOne.classList.add("form-group-1-hide");  
-      formGroupTwo.classList.remove("form-group-2-hide");
+      formGroupOne.classList.add("form-group-1-hidden");  
+      formGroupTwo.classList.remove("form-group-2-hidden");
       formGroupTwo.classList.add("form-group-2");
     });
   });
@@ -15,14 +15,14 @@ const formGroupOneAction = () => {
 
 // Form group two - mood
 const formGroupTwoAction = () => {
-  const formGroupTwo = document.querySelector("fieldset.form-group-2-hide");
+  const formGroupTwo = document.querySelector("fieldset.form-group-2-hidden");
   const radioButtonTwo = formGroupTwo.querySelectorAll(".radio__label");
-  const formGroupThree = document.querySelector("fieldset.form-group-3-hide");
+  const formGroupThree = document.querySelector("fieldset.form-group-3-hidden");
   radioButtonTwo.forEach(element => {
     element.addEventListener('click', function () {
       formGroupTwo.classList.remove("form-group-2");
-      formGroupTwo.classList.add("form-group-2-hide");
-      formGroupThree.classList.remove("form-group-3-hide");
+      formGroupTwo.classList.add("form-group-2-hidden");
+      formGroupThree.classList.remove("form-group-3-hidden");
       formGroupThree.classList.add("form-group-3");
     });  
   });
@@ -30,14 +30,14 @@ const formGroupTwoAction = () => {
 
 // Form group three - media type
 const formGroupThreeAction = () => {
-  const formGroupThree = document.querySelector("fieldset.form-group-3-hide");
+  const formGroupThree = document.querySelector("fieldset.form-group-3-hidden");
   const radioButtonThree = formGroupThree.querySelectorAll(".radio__label");
-  const formGroupFour = document.querySelector("fieldset.form-group-4-hide");
+  const formGroupFour = document.querySelector("fieldset.form-group-4-hidden");
   radioButtonThree.forEach(element => {
     element.addEventListener('click', function () {
       formGroupThree.classList.remove("form-group-3");
-      formGroupThree.classList.add("form-group-3-hide");
-      formGroupFour.classList.remove("form-group-4-hide");
+      formGroupThree.classList.add("form-group-3-hidden");
+      formGroupFour.classList.remove("form-group-4-hidden");
       formGroupFour.classList.add("form-group-4");
     });
   });
@@ -45,14 +45,14 @@ const formGroupThreeAction = () => {
 
 // Form group four - rating
 const formGroupFourAction = () => {
-  const formGroupFour = document.querySelector("fieldset.form-group-4-hide");
+  const formGroupFour = document.querySelector("fieldset.form-group-4-hidden");
   const radioButtonFour = formGroupFour.querySelectorAll(".radio__label");
-  const formGroupFive = document.querySelector("fieldset.form-group-5-hide");
+  const formGroupFive = document.querySelector("fieldset.form-group-5-hidden");
   radioButtonFour.forEach(element => {
     element.addEventListener('click', function () {
       formGroupFour.classList.remove("form-group-4");
-      formGroupFour.classList.add("form-group-4-hide");
-      formGroupFive.classList.remove("form-group-5-hide");
+      formGroupFour.classList.add("form-group-4-hidden");
+      formGroupFive.classList.remove("form-group-5-hidden");
       formGroupFive.classList.add("form-group-5");
     });
   });
@@ -60,175 +60,17 @@ const formGroupFourAction = () => {
 
 // Form group five - year
 const formGroupFiveAction = () => {
-  const formGroupFive = document.querySelector("fieldset.form-group-5-hide");
+  const formGroupFive = document.querySelector("fieldset.form-group-5-hidden");
   const radioButtonFive = formGroupFive.querySelectorAll(".radio__label");
-  const submitBtn = document.querySelector(".buttonsubmit-hide");
+  const submitBtn = document.querySelector(".buttonsubmit-hidden");
   radioButtonFive.forEach(element => {
     element.addEventListener('click', function () {
       formGroupFive.classList.remove("form-group-5");
-      formGroupFive.classList.add("form-group-5-hide");
-      submitBtn.classList.remove("buttonsubmit-hide");
+      formGroupFive.classList.add("form-group-5-hidden");
+      submitBtn.classList.remove("buttonsubmit-hidden");
       submitBtn.classList.add("buttonsubmit");
     });
   });
 };
 
-
-const myFunction = () => {
-  const drawerTransitioner = transitionHiddenElement({
-    element: document.querySelector('.js-drawer'),
-    visibleClass: 'is-open'
-  });
-  
-  document.querySelector('.js-open-button').addEventListener('click', () => {
-    drawerTransitioner.show()
-  });
-  
-  document.querySelector('.js-close-button').addEventListener('click', () => {
-    drawerTransitioner.hide()
-  });
-  
-  /** 
-   * Library code
-   * Using https://www.npmjs.com/package/@cloudfour/transition-hidden-element
-   */
-  
-  function transitionHiddenElement({
-    element,
-    visibleClass,
-    waitMode = 'transitionend',
-    timeoutDuration,
-    hideMode = 'hidden',
-    displayValue = 'block'
-  }) {
-    if (waitMode === 'timeout' && typeof timeoutDuration !== 'number') {
-      console.error(`
-        When calling transitionHiddenElement with waitMode set to timeout,
-        you must pass in a number for timeoutDuration.
-      `);
-  
-      return;
-    }
-  
-    // Don't wait for exit transitions if a user prefers reduced motion.
-    // Ideally transitions will be disabled in CSS, which means we should not wait
-    // before adding `hidden`.
-    if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
-      waitMode = 'immediate';
-    }
-  
-    /**
-     * An event listener to add `hidden` after our animations complete.
-     * This listener will remove itself after completing.
-     */
-    const listener = e => {
-      // Confirm `transitionend` was called on  our `element` and didn't bubble
-      // up from a child element.
-      if (e.target === element) {
-        applyHiddenAttributes();
-  
-        element.removeEventListener('transitionend', listener);
-      }
-    };
-  
-    const applyHiddenAttributes = () => {
-      if (hideMode === 'display') {
-        element.style.display = 'none';
-      } else {
-        element.setAttribute('hidden', true);
-      }
-    }
-  
-    const removeHiddenAttributes = () => {
-      if (hideMode === 'display') {
-        element.style.display = displayValue;
-      } else {
-        element.removeAttribute('hidden');
-      }
-    }
-  
-    return {
-      /**
-       * Show the element
-       */
-      show() {
-        /**
-         * This listener shouldn't be here but if someone spams the toggle
-         * over and over really fast it can incorrectly stick around.
-         * We remove it just to be safe.
-         */
-        element.removeEventListener('transitionend', listener);
-  
-        /**
-         * Similarly, we'll clear the timeout in case it's still hanging around.
-         */
-        if (this.timeout) {
-          clearTimeout(this.timeout);
-        }
-  
-        removeHiddenAttributes();
-  
-        /**
-         * Force a browser re-paint so the browser will realize the
-         * element is no longer `hidden` and allow transitions.
-         */
-        const reflow = element.offsetHeight;
-  
-        element.classList.add(visibleClass);
-      },
-  
-      /**
-       * Hide the element
-       */
-      hide() {
-        if (waitMode === 'transitionend') {
-          element.addEventListener('transitionend', listener);
-        } else if (waitMode === 'timeout') {
-          this.timeout = setTimeout(() => {
-            applyHiddenAttributes();
-          }, timeoutDuration);
-        } else {
-          applyHiddenAttributes();
-        }
-  
-        // Add this class to trigger our animation
-        element.classList.remove(visibleClass);
-      },
-  
-      /**
-       * Toggle the element's visibility
-       */
-      toggle() {
-        if (this.isHidden()) {
-          this.show();
-        } else {
-          this.hide();
-        }
-      },
-  
-      /**
-       * Tell whether the element is hidden or not.
-       */
-      isHidden() {
-        /**
-         * The hidden attribute does not require a value. Since an empty string is
-         * falsy, but shows the presence of an attribute we compare to `null`
-         */
-        const hasHiddenAttribute = element.getAttribute('hidden') !== null;
-  
-        const isDisplayNone = element.style.display === 'none';
-  
-        const hasVisibleClass = [...element.classList].includes(visibleClass);
-  
-        return hasHiddenAttribute || isDisplayNone || !hasVisibleClass;
-      },
-  
-      // A placeholder for our `timeout`
-      timeout: null
-    };
-  }
-  
-}
-
-export { formGroupOneAction, formGroupTwoAction, formGroupThreeAction, formGroupFourAction, formGroupFiveAction, myFunction };
-
+export { formGroupOneAction, formGroupTwoAction, formGroupThreeAction, formGroupFourAction, formGroupFiveAction };
