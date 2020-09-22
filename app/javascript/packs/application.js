9740a38794e233d0c03d00a23d118a687e66762c
Michael Knott <knottweb@gmail.com>
2020-09-22 19:44:01 +0100

    Cube moved to loading screen

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,6 @@ import bookmark from "../components/bookmark";
 import watched from "../components/watched";
 import excluded from "../components/excluded";
 import { tns } from "../../../node_modules/tiny-slider/src/tiny-slider";
-import * as THREE from 'three';
 import { formGroupOneAction, formGroupTwoAction, formGroupThreeAction, formGroupFourAction, formGroupFiveAction } from '../plugins/surveyChanger';
 import {loadingFunction } from "../plugins/loading.js";
 
@@ -117,44 +116,4 @@ document.addEventListener('turbolinks:load', () => {
     }
   });
   }
-
-  // Below code is for the spinning cube via three.js
-  // guard clause for if the element with class name "cube" exists, only then will it execute the code
-  if (document.querySelector(".cube")) {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.5, 1000);
-    const renderer = new THREE.WebGLRenderer({ alpha: true});
-    renderer.setClearColor(0x000000, 0)
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.querySelector(".cube").appendChild(renderer.domElement);
-
-    const geometry = new THREE.BoxGeometry(2, 2, 2);
-    const cubeMaterials =
-      [
-        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('https://res.cloudinary.com/dinzvmaaw/image/upload/v1599908578/NextBinge/netflix_cube_logo_eqgv1s.jpg'), side: THREE.DoubleSide }),
-        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('https://res.cloudinary.com/dinzvmaaw/image/upload/v1599908578/NextBinge/netflix_cube_logo_eqgv1s.jpg'), side: THREE.DoubleSide }),
-        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('https://res.cloudinary.com/dinzvmaaw/image/upload/v1599908578/NextBinge/amazon_cube_logo_ygdhcp.jpg'), side: THREE.DoubleSide }),
-        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('https://res.cloudinary.com/dinzvmaaw/image/upload/v1599908578/NextBinge/amazon_cube_logo_ygdhcp.jpg'), side: THREE.DoubleSide }),
-        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('https://res.cloudinary.com/dinzvmaaw/image/upload/v1599908578/NextBinge/disney_cube_logo_ogsp6r.jpg'), side: THREE.DoubleSide }),
-        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('https://res.cloudinary.com/dinzvmaaw/image/upload/v1599908578/NextBinge/disney_cube_logo_ogsp6r.jpg'), side: THREE.DoubleSide }),
-      ];
-    const material = new THREE.MeshFaceMaterial(cubeMaterials);
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-
-    camera.position.x = 0;
-    camera.position.y = 0;
-    camera.position.z = 3;
-
-    function animate() {
-      requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      cube.rotation.z += 0.01;
-
-      renderer.render(scene, camera);
-    }
-    animate();
-  }
-  // three.js code ends
 });
